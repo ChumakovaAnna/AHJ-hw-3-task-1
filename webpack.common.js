014ceffd985794329ae78119287c75db68f7e46a
Anna Chumakova <chumakova3008@gmail.com>
2020-08-02 15:52:35 +0500

    Добавлен новый курсор на гоблина

--- webpack.common.js
+++ webpack.common.js
@@ -28,7 +28,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|svg)$/i,
         loader: "file-loader",
         options: {
           name: "src/img/[name].[ext]",
